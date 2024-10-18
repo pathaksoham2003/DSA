e9f63f810fcf350aebcc4a8b5bdec6a9bef48c80
lalit_h <lalithireofficial@gmail.com>
2024-10-18 21:01:39 +0530

    Star Pattern 'Diamond'

---
+++ Patterns/Diamond.py
@@ -0,0 +1,32 @@
+#     Diamond star pattern
+#     output like:
+
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+#  * * * *
+#   * * *
+#    * *
+#     *
+
+
+n = 5
+# n=int(input("Enter height :"))
+
+for i in range(n):
+    for j in range(n, i, -1):
+        print(" ", end="")
+    for k in range(i ):
+        print("* ", end="")
+    print()
+
+for i in range(n):
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n , i , -1):
+        print("* ", end="")
+    print()
+    
+
